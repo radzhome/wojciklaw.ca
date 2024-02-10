8b64aaec3f56c8332f29bb4169a385defdfc74d6
rad <radzhome@gmail.com>
2024-02-10 18:09:58 -0500

    fix mobile views

--- tools/update_footer.py
+++ tools/update_footer.py
@@ -7,9 +7,9 @@ python3 update_footer.py
 """
 from update_func import update_section
 
-# Define the new content
+# Define the new content dropped pt-5 mt-5 from container-fluid
 footer = """
-    <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container-fluid bg-dark text-light footer wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
